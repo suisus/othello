5b14120162454f7333e670c57b712d7f0129b594
suisus <docomowenjia@gmail.com>
2024-04-23 21:36:45 +0900

    おせろ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,12 @@ const Home = () => {
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    newBoard[y][x] = turnColor;
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+
+    if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
+      setTurnColor(3 - turnColor);
+      newBoard[y][x] = turnColor;
     }
+
     setBoard(newBoard);
   };
   return (
