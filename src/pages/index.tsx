bc0c255bdeca4a4ef29b3231e0cba46c7df7cf26
suisus <docomowenjia@gmail.com>
2024-05-01 21:37:08 +0900

    端

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
+const directions = [
+  [0, 1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
@@ -13,35 +24,68 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
   const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
+    console.log(y, x);
     const newBoard = structuredClone(board);
 
-    if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
-      newBoard[y][x] = turnColor;
-    } else if (board[y + 2] !== undefined && board[y + 2][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
-      newBoard[y][x] = turnColor;
-    } else if (board[y + 3] !== undefined && board[y + 3][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
-      newBoard[y][x] = turnColor;
-    } else if (board[y + 4] !== undefined && board[y + 4][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
-      newBoard[y][x] = turnColor;
-    } else if (board[y + 5] !== undefined && board[y + 5][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
-      newBoard[y][x] = turnColor;
-    } else if (board[y + 6] !== undefined && board[y + 6][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
-      newBoard[y][x] = turnColor;
-    } else if (board[y + 7] !== undefined && board[y + 7][x] === 3 - turnColor) {
-      setTurnColor(3 - turnColor);
-      newBoard[y][x] = turnColor;
+    if (board[y][x] !== 0) return;
+
+    for (const [dy, dx] of directions) {
+      // let nx = x + dx;
+      // let ny = y + dy;
+      let findOpponent: boolean = false;
+      let i = 0;
+      while (i < 8) {
+        i++;
+        console.log(y, x);
+        console.log(i);
+        const stone = board[y + dy * i][x + dx * i];
+
+        if (stone === 0) break;
+        if (stone === 3 - turnColor) {
+          findOpponent = true;
+          continue;
+        }
+        if (stone === turnColor) {
+          if (findOpponent) {
+            newBoard[y][x] = turnColor;
+            setTurnColor(3 - turnColor);
+          } else {
+            break;
+          }
+        }
+      }
     }
+
+    // if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
+    //   for (let n = 1; y + n < board.length; n++) {
+    //     console.log(n);
+    //     if (board[y + n] !== undefined && board[y + n][x] === turnColor) {
+    //       while (n > 0) {
+    //         newBoard[y + n][x] = turnColor;
+    //         n--;
+    //       }
+    //       break;
+    //     }
+    //   }
+
+    //   newBoard[y][x] = turnColor;
+    //   console.log(newBoard);
+    //   setBoard(newBoard);
+    //   setTurnColor(3 - turnColor);
+    // }
+
+    // if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
+    // //   if (board[y + 2] !== undefined && board[y + 2][x] === turnColor) {
+    //     // newBoard[y][x] = turnColor;
+    //     // setTurnColor(3 - turnColor);
+    // //   }
+    // }
 
     setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
